0dd7dd9afed3a268345517573ef68f235d5935cb
bowo rahayu <rahayuributwibowo@gmail.com>
2026-05-23 20:27:07 +0700

    exclude midtrans notification from csrf validation
    - add `midtrans/notification` to the CSRF token validation exceptions list to allow external webhook requests to bypass verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(except: [
             'duitku/callback',
+            'midtrans/notification',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
