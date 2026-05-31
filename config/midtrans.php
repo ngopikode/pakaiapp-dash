dfa703c3d0d6490a0554b08cc8c07252eb615597
bowo rahayu <rahayuributwibowo@gmail.com>
2026-05-31 19:25:06 +0700

    feat: add configurable toggle for Midtrans payment gateway integration

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,8 +1,11 @@
 <?php
 
+$enabled = filter_var(env('MIDTRANS_ENABLED', true), FILTER_VALIDATE_BOOLEAN);
+
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', ''),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', ''),
+    'enabled' => $enabled,
+    'server_key' => $enabled ? env('MIDTRANS_SERVER_KEY', '') : '',
+    'client_key' => $enabled ? env('MIDTRANS_CLIENT_KEY', '') : '',
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => env('MIDTRANS_IS_SANITIZED', true),
     'is_3ds' => env('MIDTRANS_IS_3DS', true),
