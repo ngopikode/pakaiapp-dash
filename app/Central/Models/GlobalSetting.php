53d6b7fc7399a331ec43326704a1dfcb8b4a79c0
bowo rahayu <rahayuributwibowo@gmail.com>
2026-07-23 17:32:07 +0700

    fix: use correct #[Connection] attribute for GlobalSetting model

--- app/Central/Models/GlobalSetting.php
+++ app/Central/Models/GlobalSetting.php
@@ -2,11 +2,13 @@
 
 namespace App\Central\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Connection;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
 
-#[Table(connection: 'mysql', key: 'key', incrementing: false, keyType: 'string')]
+#[Connection('mysql')]
+#[Table(key: 'key', incrementing: false, keyType: 'string')]
 #[Fillable([
     'key',
     'value',
@@ -20,17 +22,12 @@ class GlobalSetting extends Model
      */
     public function getCastValueAttribute()
     {
-        switch ($this->type) {
-            case 'integer':
-                return (int) $this->value;
-            case 'float':
-                return (float) $this->value;
-            case 'boolean':
-                return filter_var($this->value, FILTER_VALIDATE_BOOLEAN);
-            case 'json':
-                return json_decode($this->value, true);
-            default:
-                return $this->value;
-        }
+        return match ($this->type) {
+            'integer' => (int)$this->value,
+            'float' => (float)$this->value,
+            'boolean' => filter_var($this->value, FILTER_VALIDATE_BOOLEAN),
+            'json' => json_decode($this->value, true),
+            default => $this->value,
+        };
     }
 }
